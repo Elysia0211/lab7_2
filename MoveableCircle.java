758d2b613e37ee0d760260cf93bce51764053443
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:52:33 +0700

    Create MoveableCircle.java

---
+++ MoveableCircle.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7_2;
+
+/**
+ *
+ * @author Admin
+ */
+public class MoveableCircle implements Moveable{
+    int radius;
+    
+    @Override
+    public void moveUp() {
+    }
+
+    @Override
+    public void moveDown() {
+    }
+
+    @Override
+    public void moveLeft() {
+    }
+
+    @Override
+    public void moveRight() {
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+    
+}
