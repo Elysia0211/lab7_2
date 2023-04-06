06deb1ed530ae064c8b8254c8168ba52949b2303
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:51:53 +0700

    Create Moveable.java

---
+++ Moveable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7_2;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Moveable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+    
+}
