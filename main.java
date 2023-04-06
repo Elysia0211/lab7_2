636b642f2ee2d2e1d9ba46e1160fa6374c5c7cd4
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:51:14 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7_2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab7_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MoveableCircle mv = new MoveableCircle();
+        mv.moveDown();
+        mv.moveLeft();
+        mv.moveRight();
+        mv.moveUp();
+        
+    }
+    
+}
